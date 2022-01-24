920bdc8063546372a4c2374893d34530e65408d1
Ferran <63797008+fanglada@users.noreply.github.com>
2022-01-24 18:59:16 +0100

    Primer Commit

---
+++ src/main/java/dam2/jaf/App.java
@@ -0,0 +1,13 @@
+package dam2.jaf;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
