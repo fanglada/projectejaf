9bf473f828918b7f4221b408e34d6a15f9af6fed
AlbaGA <91140869+AlbaGA@users.noreply.github.com>
2022-01-24 20:05:56 +0100

    com1

--- src/main/java/model/Conductor.java
+++ src/main/java/model/Conductor.java
@@ -2,6 +2,19 @@ package model;
 
 public class Conductor extends Persona {
 	private int carnet;
-	
+
+	public Conductor(String dNI, String nombre, String ape1, String ape2, String fNacimiento, String telefono,
+			String direccion, String mail, int carnet) {
+		super(dNI, nombre, ape1, ape2, fNacimiento, telefono, direccion, mail);
+		this.carnet = carnet;
+	}
+
+	public int getCarnet() {
+		return carnet;
+	}
+
+	public void setCarnet(int carnet) {
+		this.carnet = carnet;
+	}
 	
 }
