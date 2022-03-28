180408adbe3d7eff1bfed1bdf3e3c19f4f283acb
jgomezinspalamos <jgomez@inspalamos.cat>
2022-03-28 19:08:27 +0200

    Implementació de Carnet

--- src/main/java/model/CarnetDAOImpl.java
+++ src/main/java/model/CarnetDAOImpl.java
@@ -1,5 +1,9 @@
 package model;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
 import dam2.jaf.Connexio;
@@ -7,25 +11,90 @@ import dam2.jaf.Connexio;
 public class CarnetDAOImpl implements CarnetDAO {
 
 	static int Tots(Connexio con, List<Carnet> carnets) {
-		return 0;
+
+		int size=0;
+		try {
+
+			String sql = "Select * from carnet;";
+			Statement stm = con.getConnexio().createStatement();
+
+			ResultSet resultSet= stm.executeQuery(sql);
+
+			while(resultSet.next()) {
+				Carnet carnet = new Carnet(resultSet.getInt("idCarnet"), resultSet.getString("descripcio"));
+				carnets.add(carnet);
+			}
+			size=carnets.size();
+		}
+
+		catch (SQLException e) 
+		{
+			e.printStackTrace();
+			return 0;
+		}
+		return size;
 	}
-	
+
 	@Override
 	public int Create(Connexio con, Carnet carnet) {
-		// TODO Auto-generated method stub
-		return 0;
+
+		int resultat = 0;
+
+		try {
+
+			PreparedStatement stm = con.getConnexio().prepareStatement("INSERT INTO Carnet VALUES (NULL,?)");				
+			stm.setString(1, carnet.getDescripcio());
+
+			stm.executeUpdate();
+
+			resultat = 1;
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}		
+		return resultat;
 	}
 
 	@Override
 	public int Update(Connexio con, Carnet carnet) {
-		// TODO Auto-generated method stub
-		return 0;
+
+		int resultat = 0;
+
+		try {
+
+			PreparedStatement stm = con.getConnexio().prepareStatement("UPDATE Carnet SET idCarnet = ?, descripcio = ?");				
+			stm.setInt(1, carnet.getIdCarnet());
+			stm.setString(2, carnet.getDescripcio());
+
+			stm.executeUpdate();
+
+			resultat = 1;
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}		
+		return resultat;
 	}
+
 
 	@Override
 	public int Delete(Connexio con, int id) {
-		// TODO Auto-generated method stub
-		return 0;
+
+		int resultat = 0;
+
+		try {
+
+			PreparedStatement stm = con.getConnexio().prepareStatement("DELETE from Carnet where idCarnet = ?");				
+			stm.setInt(1, id);
+
+			stm.executeUpdate();
+
+			resultat = 1;
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}		
+		return resultat;
 	}
 
 }
