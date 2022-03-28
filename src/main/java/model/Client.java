8e108b20e003fd5d482ba4d8753a5e5a956a0a08
Ferran <63797008+fanglada@users.noreply.github.com>
2022-03-28 18:29:59 +0200

    Client + config

--- src/main/java/model/Client.java
+++ src/main/java/model/Client.java
@@ -1,13 +1,33 @@
 package model;
 
 import java.time.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Client extends Persona{
+	
+	private List<Carnet> carnets;
 
 	public Client(String dni, String nom, String cognom1, String cognom2, LocalDate dataNaixament, String telefon,
 			String direccio, String mail) {
 		super(dni, nom, cognom1, cognom2, dataNaixament, telefon, direccio, mail);
-		// TODO Auto-generated constructor stub
+		this.carnets = new ArrayList<Carnet>();
+	}
+	
+	public List<Carnet> getCarnet() {
+		return carnets;
+	}
+
+	public void setCarnet(List<Carnet> carnets) {
+		this.carnets = carnets;
+	}
+	
+	public void addVehicles(Carnet carnet) {
+		this.carnets.add(carnet);
+	}
+	
+	public void delVehicles(Carnet carnet) {
+		this.carnets.remove(carnet);
 	}
 
 	@Override
