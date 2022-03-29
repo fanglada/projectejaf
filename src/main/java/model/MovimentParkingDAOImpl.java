3de6a5793976a1fc470c58b626a3c527c5a5ecf9
jgomezinspalamos <jgomez@inspalamos.cat>
2022-03-29 18:27:52 +0200

    Implementació MovimentParking

--- src/main/java/model/MovimentParkingDAOImpl.java
+++ src/main/java/model/MovimentParkingDAOImpl.java
@@ -1,31 +1,112 @@
 package model;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Timestamp;
 import java.util.List;
 
 import dam2.jaf.Connexio;
 
 public class MovimentParkingDAOImpl implements MovimentParkingDAO {
 
-	static int Tots(Connexio con, List<MovimentParking> movimentParkings) {
-		return 0;
+	public static int Tots(Connexio con, List<MovimentParking> movimentParkings) {
+
+		int size=0;
+		try {
+
+			String sql = "SELECT * FROM movimentParking;";
+			Statement stm = con.getConnexio().createStatement();
+
+			ResultSet resultSet= stm.executeQuery(sql);
+
+			while(resultSet.next()) {
+				//movimentParkings.add(new MovimentParking(resultSet.getInt("idMoviment"), resultSet.getInt("origen"), resultSet.getInt("desti"), resultSet.getString("matricula"), resultSet.getTimestamp("dataHora").toLocalDateTime()));
+			}
+			size=movimentParkings.size();
+		}
+
+		catch (SQLException e) 
+		{
+			e.printStackTrace();
+			return 0;
+		}
+		return size;
 	}
-	
+
 	@Override
 	public int Create(Connexio con, MovimentParking movimentParking) {
-		// TODO Auto-generated method stub
-		return 0;
+
+		int resultat = 0;
+
+		try {
+
+			PreparedStatement stm = con.getConnexio().prepareStatement("INSERT INTO MovimentParking VALUES (?,?,?,?,?)");				
+			stm.setInt(1, movimentParking.getIdMovimentParking());
+			stm.setInt(2, movimentParking.getOrigen().getIdParking());
+			stm.setInt(3, movimentParking.getDesti().getIdParking());
+			stm.setString(4, movimentParking.getVehicle().getMatricula());
+			stm.setTimestamp(5, Timestamp.valueOf(movimentParking.getDataHora()));
+
+			resultat = stm.executeUpdate();
+
+			ResultSet rst = stm.executeQuery("SELECT @@Identity AS id");
+
+			if(rst.next())
+			{
+				movimentParking.setIdMovimentParking(rst.getInt("id"));
+			}
+
+		} 
+		catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
+		return resultat;
 	}
 
 	@Override
 	public int Update(Connexio con, MovimentParking movimentParking) {
-		// TODO Auto-generated method stub
-		return 0;
+
+		int resultat = 0;
+
+		try {
+
+			PreparedStatement stm = con.getConnexio().prepareStatement("UPDATE MovimentParking SET origen = ?, desti = ?, matricula = ?, dataHora = ? WHERE idMovimentParking = ?");				
+			stm.setInt(1, movimentParking.getIdMovimentParking());
+			stm.setInt(2, movimentParking.getOrigen().getIdParking());
+			stm.setInt(3, movimentParking.getDesti().getIdParking());
+			stm.setString(4, movimentParking.getVehicle().getMatricula());
+			stm.setTimestamp(5, Timestamp.valueOf(movimentParking.getDataHora()));
+
+			resultat = stm.executeUpdate();
+
+		} 
+		catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
+		return resultat;
 	}
 
 	@Override
 	public int Delete(Connexio con, int id) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
 
+		int resultat = 0;
+
+		try {
+
+			PreparedStatement stm = con.getConnexio().prepareStatement("DELETE FROM MovimentParking WHERE idMovimentParking = ?");				
+			stm.setInt(1, id);
+
+			resultat = stm.executeUpdate();
+
+		} 
+		catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
+		return resultat;
+	}
 }
