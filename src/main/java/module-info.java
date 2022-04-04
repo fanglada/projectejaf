438221473247c969090777b21eb9f1c348684577
FMayola <francescmayola86@gmail.com>
2022-04-04 19:19:03 +0200

    controller client

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module dam2.jaf {
     requires javafx.fxml;
 	requires java.sql;
 	requires org.controlsfx.controls;
+	requires javafx.graphics;
 
     opens dam2.jaf to javafx.fxml;
     exports dam2.jaf;
