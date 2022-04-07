72a39242b30ee43d2f414ce069b0e8623e77b37d
FMayola <francescmayola86@gmail.com>
2022-04-07 22:15:46 +0200

    prova

--- src/main/java/dam2/jaf/ConductorController.java
+++ src/main/java/dam2/jaf/ConductorController.java
@@ -140,7 +140,7 @@ public class ConductorController implements Initializable{
     
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		// TODO Auto-generated method stub
+
 		App.setTitol("Conductor");
 
 		if (!taula) {
